6d034f734affd6f2612031e8fc1d9a65afd59b1e
JuliaJulia <ulana_m@ukr.net>
2022-06-30 17:12:17 +0300

    hw1 RickandMorty

--- src/App.js
+++ src/App.js
@@ -4,25 +4,47 @@ function App() {
     return (
         <div>
             <Character
-                name={'Гомер Джей Сімпсон'}
-                description={'батько родини, гладкий, лисий, ледачий і не дуже розумний. Він часто поводиться абсурдно, егоїстично і нетактовно, втім, залишається симпатичним.'}
-                image={'https://www.onthisday.com/images/people/homer-simpson-medium.jpg'}/>
+                id={1}
+                name={'Rick SanchezRick Sanchez'}
+                status={'Alive'}
+                species={'Human'}
+                gender={'Male'}
+                image={'https://rickandmortyapi.com/api/character/avatar/1.jpeg'}/>
             <Character
-                name={'Марджері «Мардж» Сімпсон'}
-                description={'дружина Гомера, дівоче ім\'я — Був\'є. Домогосподарка, майже весь час проводить вдома, доглядаючи за наймолодшою дитиною Меґґі, а також за Бартом і Лісою, і навіть за чоловіком. Її найвідоміша особливість — блакитне волосся, зібране у високу зачіску.'}
-                image={'https://www.freeiconspng.com/uploads/marge-simpson-png-11.png'}/>
+                id={2}
+                name={'Morty Smith'}
+                status={'Alive'}
+                species={'Human'}
+                gender={'Male'}
+                image={'https://rickandmortyapi.com/api/character/avatar/2.jpeg'}/>
             <Character
-                name={'Бартоломео «Барт» Сімпсон'}
-                description={'10-літній син Гомера і Мардж Сімпсон, найстарша дитина в сім\'ї, втіллення образу непослуха, бешкетника і посереднього учня в школі.'}
-                image={'https://upload.wikimedia.org/wikipedia/ru/2/29/Bart_Simpson.gif'}/>
+                id={4}
+                name={'Beth Smith'}
+                status={'Alive'}
+                species={'Human'}
+                gender={'Female'}
+                image={'https://rickandmortyapi.com/api/character/avatar/4.jpeg'}/>
             <Character
-                name={'Ліса Мері Сімпсон'}
-                description={'дочка Гомера і Мардж Сімпсон, надзвичайно розумна, не по літах розвинена, суспільно активна дівчинка. Любить джаз та грає на саксофоні, цікавиться філософією, наукою, історією. Активіст із захисту довкілля і прав тварин. Стала буддисткою і вегетаріанкою.'}
-                image={'https://citaty.info/files/characters/638.png'}/>
+                id={9}
+                name={'Agency Director'}
+                status={'Dead'}
+                species={'Human'}
+                gender={'Male'}
+                image={'https://rickandmortyapi.com/api/character/avatar/9.jpeg'}/>
             <Character
-                name={'Маргарет «Меґґі» Сімпсон'}
-                description={'найменша дочка Гомера в Мардж Сімпсон. Протягом усіх років серіалу не росте і завжди залишається немовлям у повзунках та підгузнику.'}
-                image={'https://simg.nicepng.com/png/small/138-1386769_simpsons-png-pack-maggie-simpson.png'}/>
+                id={16}
+                name={'Amish Cyborg'}
+                status={'Dead'}
+                species={'Alien'}
+                gender={'Male'}
+                image={'https://rickandmortyapi.com/api/character/avatar/16.jpeg'}/>
+            <Character
+                id={17}
+                name={'Annie'}
+                status={'Alive'}
+                species={'Human'}
+                gender={'Female'}
+                image={'https://rickandmortyapi.com/api/character/avatar/17.jpeg'}/>
         </div>
     );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,11 +1,14 @@
-function Character({name, description, image}) {
+function Character({id,name,status,species,gender,image}) {
 
     return (
         <div>
             <div>
                 <h2 className={'name'}>{name}</h2>
-                <p className={'desc'}>{description}</p>
-                <img src={image} alt=""/>
+                <h5>id number: {id}</h5>
+                <h4>status: {status}</h4>
+                <h4>species: {species}</h4>
+                <h4>gender: {gender}</h4>
+                <img src={image} alt={name}/>
             </div>
         </div>
     )
